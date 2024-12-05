45de7999ec8ddb994ab0998fe22b20a3fe80f21e
ThomasMSPCoding <t.addab@outlook.de>
2024-12-05 23:52:59 +0100

    ive added the comment explaining the node.js

--- query.js
+++ query.js
@@ -1,3 +1,12 @@
+/**Yes we use a library called node.js for our SPARQL query. To explain what node.js is, it is a real time process and event connection with the website itself
+ * It connect the query directly with the imported and exported data we aquired!
+ * Here are examples we used node.js.
+ * SPARQLQueryDispatcher Class is node.js component, this code uses the feature 'fetch' API, which is the primarily for client-side HTTP request
+ * Than the DOM manipulation method(found in the displayResults code) feature codes like getElementById or createElement
+ * All the node.js component runs the browser environment with a stable background.
+ */
+
+
 /**
  * SPARQLQueryDispatcher Class
  * This class is designed to send SPARQL queries to a specified endpoint and return the results.
